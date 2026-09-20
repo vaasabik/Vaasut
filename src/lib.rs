852642b8e9ffa25ffaa1a8d8f2072e46aa556b16
vaasabik <vaasabik@gmail.com>
2026-09-20 16:45:58 +1000

    Fix: Update web-sys for Starting page

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,21 @@ use wasm_bindgen::prelude::*;
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(start)]
 pub async fn start() {
+    // Получаем canvas элемент из DOM
+    let canvas = web_sys::window()
+        .expect("No window")
+        .document()
+        .expect("No document")
+        .get_element_by_id("vaasut_canvas")
+        .expect("No canvas element")
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .expect("Element is not a canvas");
+
     let web_options = eframe::WebOptions::default();
+    
     eframe::WebRunner::new()
         .start(
-            "vaasut_canvas",
+            canvas,
             web_options,
             Box::new(|cc| Ok(Box::new(vaasut_editor::VaasutEditor::new(cc)))),
         )
